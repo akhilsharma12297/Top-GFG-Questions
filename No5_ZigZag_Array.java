b5f7701a25168a33214954092053972c3b356a60
Akhil sharma <37407049+akhilsharma12297@users.noreply.github.com>
2019-03-14 20:20:56 +0530

    Top GFG Questions

---
+++ No5_ZigZag_Array.java
@@ -0,0 +1,12 @@
+package Interview_Prep;
+
+public class No5_ZigZag_Array {
+
+	public static void main(String[] args) {
+		
+		int [] arr = {4, 3, 7, 8, 6, 2, 1};
+		// TODO Auto-generated method stub
+
+	}
+
+}
