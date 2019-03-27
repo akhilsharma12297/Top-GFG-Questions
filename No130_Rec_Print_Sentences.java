a5253e0f5c183a6adc80ff03a43de08e3c67b808
Akhil sharma <37407049+akhilsharma12297@users.noreply.github.com>
2019-03-27 19:52:18 +0530

    Code

--- No130_Rec_Print_Sentences.java
+++ No130_Rec_Print_Sentences.java
@@ -6,18 +6,24 @@ public class No130_Rec_Print_Sentences {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String arr[][]  = {
-							{"you", "we"}, 
-							{"have", "are"}, 
-							{"sleep", "eat", "drink"}
-							};
-		
+		String arr[][] = { { "you", "we" }, { "have", "are" }, { "sleep", "eat", "drink" } };
 
 	}
 
-	public static void func(String arr[]C])
-	{
-		
+	public static void func(String arr[][]) {
+
+		for (int i = 0; i < arr[0].length; i++) {
+
+			for (int j = 0; j < arr[0].length; j++) {
+
+				for (int k = 0; k < arr[0].length; k++) {
+
+				}
+
+			}
+
+		}
+
 	}
 
 }
