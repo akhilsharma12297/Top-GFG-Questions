71a37f3acd81175768be31d5bc75ac23800e6daf
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-05-01 14:57:48 +0530

    Code

--- Linked_List/No281_Random_Pointer_LL_Copy.java
+++ Linked_List/No281_Random_Pointer_LL_Copy.java
@@ -1,13 +1,59 @@
 package Linked_List;
 
-public class No281_Random_Pointer_LL_Copy{
+public class No281_Random_Pointer_LL_Copy {
 
 	class Node {
 		Node next;
-		int chat;
+		Node random;
+		int data;
 	}
 
+	static int size = 0;
 	static Node head;
-	static int size;
+	static Node tail;
+
+	public No281_Random_Pointer_LL_Copy cloneIT() {
+
+		Node curr = head;
+
+		while (curr != null) {
+
+			Node temp = new Node();
+			temp.data = curr.data;
+			temp.next = curr.next;
+			curr.next = temp;
+
+			curr = curr.next.next;
+
+		}
+
+		curr = head;
+
+		while (curr != null) {
+
+			curr.next.random = curr.random.next;
+
+			curr = curr.next.next;
+
+		}
+
+		curr = head;
+		Node copy = head.next;
+
+		No281_Random_Pointer_LL_Copy list = new No281_Random_Pointer_LL_Copy();
+
+		list.head = copy;
+
+		while (curr != null && copy != null) {
+
+			curr.next = curr.next.next;
+
+			copy.next = copy.next.next;
+
+		}
+
+		return list;
+
+	}
 
 }
