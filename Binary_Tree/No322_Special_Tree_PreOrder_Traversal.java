3d35c5097f5d6e2010bf879fb73ef8471a0124a3
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-05-06 17:18:13 +0530

    Code

---
+++ Binary_Tree/No322_Special_Tree_PreOrder_Traversal.java
@@ -0,0 +1,44 @@
+package Binary_Tree;
+
+import java.util.Stack;
+
+public class No322_Special_Tree_PreOrder_Traversal {
+	class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+
+	static int size;
+	static Node root;
+
+	static int myidx = 0;
+
+	public void SpecialTree(int[] Pre_values, char[] Pre_nodes) {
+
+		int idx = 0;
+
+		SpecialTreehelper(Pre_values, Pre_nodes, myidx, Pre_values.length, root);
+
+	}
+
+	private Node SpecialTreehelper(int[] pre_values, char[] pre_nodes, int myidx, int length, Node node) {
+
+		if (myidx == length) {
+			return null;
+		}
+
+		node = new Node();
+		node.data = pre_values[myidx];
+		myidx++;
+
+		if (pre_nodes[myidx] == 'N') {
+
+			node.left = SpecialTreehelper(pre_values, pre_nodes, myidx, length, node.left);
+
+			node.left = SpecialTreehelper(pre_values, pre_nodes, myidx, length, node.left);
+		}
+		return node;
+	}
+
+}
