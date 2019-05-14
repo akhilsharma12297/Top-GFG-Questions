d79ab090dfd4f07c4b2be70d2b06a007b7d45967
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-05-14 16:11:16 +0530

    Code

--- Binary_Tree/No361_Serialize_Deserialize.java
+++ Binary_Tree/No361_Serialize_Deserialize.java
@@ -1,8 +1,9 @@
 package Binary_Tree;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
-public class No356_Max_Sum_Path_across_Tree {
+public class No361_Serialize_Deserialize {
 
 	class Node {
 		int data;
@@ -13,9 +14,9 @@ public class No356_Max_Sum_Path_across_Tree {
 	static Node root;
 	static int size;
 
-	public No356_Max_Sum_Path_across_Tree(int[] arr) {
+	public No361_Serialize_Deserialize(int[] arr) {
 
-		Stack<Node> stack = new Stack<No356_Max_Sum_Path_across_Tree.Node>();
+		Stack<Node> stack = new Stack<No361_Serialize_Deserialize.Node>();
 
 		for (int val : arr) {
 
@@ -72,15 +73,42 @@ public class No356_Max_Sum_Path_across_Tree {
 
 	}
 
+	public void createArray() {
+
+		ArrayList<Integer> idx = new ArrayList<Integer>();
+		createArray(root, idx);
+		idx.add(-1);
+
+		System.out.println(idx);
+	}
+
+	private void createArray(Node node, ArrayList<Integer> idx) {
+
+		if (node == null) {
+			return;
+		}
+
+		idx.add(node.data);
+
+		createArray(node.left, idx);
+
+		createArray(node.right, idx);
+
+		idx.add(-1);
+
+	}
+
 	public static void main(String[] args) {
 
 		int[] arr = { 50, 25, 12, -1, 73, -1, -1, 75, 62, -1, 87, -1, -1, -1 };
 
-		// int[] arr = { 50, 25, 12, -1, 37, 30, -1, 40, -1, -1, -1, 75, 62, 60, -1, 70,
-		// -1, -1, 87, -1, -1, -1 };
-		No356_Max_Sum_Path_across_Tree bt = new No356_Max_Sum_Path_across_Tree(arr);
+		No361_Serialize_Deserialize bt = new No361_Serialize_Deserialize(arr);
 
 		bt.display();
+
+		System.out.println();
+
+		bt.createArray();
 
 		System.out.println();
 
