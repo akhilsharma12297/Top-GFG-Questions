c0b4383c9adff1d6c3e2557c55547b0cbd36d2e1
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-06-15 19:51:38 +0530

    Code

---
+++ Binary_Search_Tree/No364_BST_from_PreOrder.java
@@ -0,0 +1,27 @@
+package Binary_Search_Tree;
+
+public class No364_BST_from_PreOrder {
+
+	class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+
+	static Node root;
+
+	public void func(int[] pres) {
+
+	}
+
+	public static void main(String[] args) {
+
+		int pre[] = new int[] { 10, 5, 1, 7, 40, 50 };
+
+		No364_BST_from_PreOrder bst = new No364_BST_from_PreOrder();
+
+		bst.func(pres);
+		
+
+	}
+}
--- Linked_List/No270_Intersection.java
+++ Linked_List/No270_Intersection.java
@@ -1,5 +1,6 @@
 package Linked_List;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class No270_Intersection {
@@ -14,6 +15,7 @@ public class No270_Intersection {
 
 	static int ans = 0;
 
+	// Method-1
 	public void Intersection() {
 
 		Intersection(Head, Head2);
@@ -35,6 +37,7 @@ public class No270_Intersection {
 		}
 	}
 
+	// Method-2
 	public void Intersection_visted() {
 		HashSet<Integer> visited = new HashSet<Integer>();
 
@@ -65,4 +68,68 @@ public class No270_Intersection {
 		}
 
 	}
+
+	// Method-3
+	// Gives all the nodes int the intersection
+	public static Node findIntersection(Node n1, Node n2) {
+		HashMap<Integer, Node> map = new HashMap<>();
+		HashSet<Integer> st = new HashSet<>();
+		while (n1 != null) {
+			st.add(n1.data);
+			n1 = n1.next;
+		}
+		while (n2 != null) {
+			if (st.contains(n2.data)) {
+				map.put(n2.data, n2);
+			}
+			n2 = n2.next;
+
+		}
+		Node head = null;
+		Node prev = null;
+		for (int val : map.keySet()) {
+			Node cur = map.get(val);
+			if (head == null) {
+				head = cur;
+				prev = cur;
+				cur.next = null;
+			} else {
+				cur.next = null;
+				prev.next = cur;
+				prev = cur;
+
+			}
+		}
+		return insertionSortList(head);
+	}
+
+	static Node result;
+
+	public static Node insertionSortList(Node head) {
+		result = null;
+		Node cur = head;
+
+		while (cur != null) {
+			Node nt = cur.next;
+			insert(cur);
+			cur = nt;
+		}
+		return result;
+	}
+
+	public static void insert(Node add) {
+		if (result == null || result.data >= add.data) {
+			add.next = result;
+			result = add;
+		} else {
+			Node cur = result;
+			while (cur.next != null && cur.next.data < add.data) {
+				cur = cur.next;
+			}
+			add.next = cur.next;
+			cur.next = add;
+		}
+
+	}
+
 }
