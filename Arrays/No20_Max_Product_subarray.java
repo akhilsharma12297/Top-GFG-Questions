86e37a99eaac8e90b52737ed35b8a49f6a058e9f
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-07-11 12:37:48 +0530

    Code

--- Arrays/No20_Max_Product_subarray.java
+++ Arrays/No20_Max_Product_subarray.java
@@ -5,7 +5,6 @@ package Arrays;
 public class No20_Max_Product_subarray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int arr[] = { -2, -3, 0, -2, -40 };
 
@@ -14,31 +13,22 @@ public class No20_Max_Product_subarray {
 	}
 
 	public static void func(int[] arr) {
-
-		int maxsum = 0;
-		int sum = 1;
-		int start = 0;
-		int end = 0;
+		int min = arr[0];
+		int max = arr[0];
+		int ans = 0;
 
 		for (int i = 0; i < arr.length; i++) {
-			sum = arr[i];
-			for (int j = i + 1; j < arr.length; j++) {
 
-				sum = sum * arr[j];
+			int tempmax = max;
 
-				if (sum > maxsum) {
+			max = Math.max(Math.max(min * arr[i], max * arr[i]), arr[i]);
+			min = Math.min(Math.min(tempmax * arr[i], min * arr[i]), arr[i]);
 
-					maxsum = sum;
-					start = i;
-					end = j;
+			ans = Math.max(max, ans);
 
-				}
-
-			}
 		}
 
-		System.out.println(maxsum + " " + start + " " + end);
-
+		System.out.println(ans);
 	}
 
 }
